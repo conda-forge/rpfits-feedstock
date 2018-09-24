89dc94bac0a846b44ccc9ba69169dfab65e1a90f
Peter Williams <peter@newton.cx>
2018-09-24 13:40:54 -0400

    Update build infrastructure for new multi-toolchain setup.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,26 +1,21 @@
 #! /bin/bash
 
-# The invocation of this script is preceded by a `source activate` of the
-# Conda environment. In conda ~ 4.2 (at least 4.2.13, probably other
-# versions), the activate script can reset the important $IFS variable if
-# there are any files in $PREFIX/etc/conda/activate.d. The `toolchain` package
-# installs just such a file, and therefore this script ends up with a
-# clobbered $IFS. So:
-IFS=$' \t\n'
+set -ex
 
-set -e
-
-# Sigh, the easiest option is to just compile it all ourselves.
+# Sigh, the RPFITS build system is lame and the easiest option is to just
+# compile it all ourselves. But first we need to abstract across platforms via
+# the following variables:
 #
-# The preset $CFLAGS are all ones that can/should be passed to gfortran.
-
-FC=gfortran
-FFLAGS="-g -O -fno-automatic -Wall -fPIC $CFLAGS"
-CFLAGS="-g -O -Wall -fPIC $CFLAGS"
-
-mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/include
+# - $CC, the C compiler (this is preset on all platform/toolchain combos)
+# - $CFLAGS, generic options to $CC (ditto)
+# - $FC, the FORTRAN compiler
+# - $FFLAGS, generic options to $FC
+# - $SOFLAGS, options to $FC for making a shared library
+# - $SOEXT, the filename extension of the output shared library
+# - $EXEFLAGS, options to $CC for making an executable that links to the shlib
 
 if [ -n "$OSX_ARCH" ] ; then
+    # macOS
     SOEXT=dylib # see other choice; there's a reason we're not using $SHLIB_EXT
     SOFLAGS=(
 	-dynamiclib
@@ -33,15 +28,21 @@ if [ -n "$OSX_ARCH" ] ; then
 	-dynamic
     )
 
-    # See pgplot-feedstock:recipe/build.sh for an explanation of what's going
-    # on here. A bad path in the `gcc` package propagates into our dylib
-    # without this workaround.
-    for name in gcc_ext.10.4 gcc_ext.10.5 ; do
-        badlib="$PREFIX/lib/lib${name}.dylib"
-        rm -f "$badlib"
-        ln -s "libgcc_s.1.dylib" "$badlib"
-    done
+    if [ "$c_compiler" = toolchain_c ] ; then
+        # macOS toolchain_c / toolchain_fort compilers
+        # - CC, CFLAGS already set
+        FC=gfortran
+        FFLAGS="-g -O -fno-automatic -Wall -fPIC -m$ARCH -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+    elif [ "$c_compiler" = clang ] ; then
+        # Linux clang / gfortran compilers
+        # - CC, CFLAGS, FC, FFLAGS already set
+        :
+    else
+        echo >&2 "ERROR: unrecognized macOS C compiler \"$c_compiler\""
+        exit 1
+    fi
 else
+    # Linux
     SOEXT=so.0 # note: the ".0" makes this not the same as $SHLIB_EXT
     SOFLAGS=(
 	-shared
@@ -49,14 +50,34 @@ else
 	-Wl,-soname,librpfits.so.0
     )
     EXEFLAGS=()
+
+    if [ "$c_compiler" = toolchain_c ] ; then
+        # Linux toolchain_c / toolchain_fort compilers
+        # - CC, CFLAGS already set
+        FC=gfortran
+        FFLAGS="-g -O -fno-automatic -Wall -fPIC -m$ARCH"
+    elif [ "$c_compiler" = gcc ] ; then
+        # Linux gcc / gfortran compilers
+        # - CC, CFLAGS, FC, FFLAGS already set
+        :
+    else
+        echo >&2 "ERROR: unrecognized Linux C compiler \"$c_compiler\""
+        exit 1
+    fi
 fi
+
+# Now we can build.
+
+mkdir -p $PREFIX/bin $PREFIX/lib $PREFIX/include
+
+$CC $CFLAGS -o utdate.o -c code/utdate.c
 
 $FC "${SOFLAGS[@]}" $FFLAGS \
     -o $PREFIX/lib/librpfits.$SOEXT \
-    code/*.f code/utdate.c code/darwin/*.f
+    code/*.f code/darwin/*.f utdate.o
 
 for bin in rpfex rpfhdr ; do
-    gcc "${EXEFLAGS[@]}" \
+    $CC "${EXEFLAGS[@]}" $CFLAGS \
 	-o $PREFIX/bin/$bin \
 	code/$bin.c $PREFIX/lib/librpfits.$SOEXT
 done
